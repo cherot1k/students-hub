be491d1b6546c0e1d984da9c3be5952695495852
Konstantin Osypenko <osk@unicsoft.com>
2021-09-25 18:40:36 +0300

    make it better

--- modules/db/config.js
+++ modules/db/config.js
@@ -1,12 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_NAME, process.env.POSTGRES_PASSWORD, {
-  host: process.env.POSTGRES_HOSTNAME,
-  dialect: 'postgres',
-  define: {
-    timestamps: false
-  },
-  omitNull: true
-})
+// const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_NAME, process.env.POSTGRES_PASSWORD, {
+//   host: process.env.POSTGRES_HOSTNAME,
+//   dialect: 'postgres',
+//   define: {
+//     timestamps: false
+//   },
+//   omitNull: true
+// })
+
+const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 module.exports = sequelize
